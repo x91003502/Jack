7838deaeb21f87d46527392416b6fa7810b49281
x91003502 <x91003502@gmail.com>
2021-05-03 23:54:35 +0800

    Created avl.py, test_avl.py.

--- dsa/basic_data_structures/avl.py
+++ dsa/basic_data_structures/avl.py
@@ -66,6 +66,7 @@ class AVL(object):
         return self.recur_find(self.root, key)
     
     def recur_find(self, croot, key):
+        if key == croot.key: return croot
         if key < croot.key and croot.left is not None:
             return self.recur_find(croot.left, key)
         elif key < croot.key and croot.left is None:
@@ -77,136 +78,6 @@ class AVL(object):
         elif key > croot.key and croot.right is None:
             print(f'key {key} not found, return a parent node {croot.key}')
             return croot
-        
-        if croot.left.key == key: return croot.left
-        if croot.right.key == key: return croot.right
-    
-    def remove(self, key):
-        croot = self.find(key)
-        if croot is None:
-            print('key not found')
-            return
-        if croot.left is not None and croot.right is not None:
-            self.two_child_remove(croot)
-        else:
-            self.zero_one_child_remove(croot)
-    
-    def two_child_remove(self, croot):
-        iop = self.right_most_child(croot.left)
-        croot.key = iop.key
-        croot.value = iop.value
-        self.zero_one_child_remove(iop)
-    
-    def zero_one_child_remove(self, croot):
-        parent = croot.parent
-        if parent.left is not None and parent.left.key == croot.key:
-            if croot.left is None:
-                parent.left = croot.right
-            else:
-                parent.left = croot.left
-        elif parent.right is not None and parent.right.key == croot.key:
-            if croot.left is None:
-                parent.right = croot.right
-            else:
-                parent.right = croot.left
-    
-    def right_most_child(self, croot):
-        if croot.right is None: return croot
-        else: return self.right_most_child(croot.right)
-        
-    def pre_order(self):
-        res = list()
-        return self.recur_pre_order(self.root, res)
-    
-    def recur_pre_order(self, croot, res):
-        if croot is not None:
-            res.append(croot.key)
-            # print(croot.key)
-            self.recur_pre_order(croot.left, res)
-            self.recur_pre_order(croot.right, res)
-        return res
-    
-    def in_order(self):
-        res = list()
-        return self.recur_in_order(self.root, res)
-    
-    def recur_in_order(self, croot, res):
-        if croot is not None:
-            self.recur_in_order(croot.left, res)
-            res.append(croot.key)
-            # print(croot.key)
-            self.recur_in_order(croot.right, res)
-        return res
-    
-    def level_order(self):
-        if self.root is None: return
-        queue = list()
-        res = list()
-        queue.append(self.root)
-        while len(queue) != 0:
-            node = queue.pop(0)
-            # print(node.key)
-            res.append(node.key)
-            if node.left is not None:
-                queue.append(node.left)
-            if node.right is not None:
-                queue.append(node.right)
-        return res
-    
-    def height(self):
-        if self.root is None: return -1
-        else: return self.recur_height(self.root)
-    
-    def recur_height(self, croot):
-        if croot is None: return -1
-        else: return 1 + max(self.recur_height(croot.left), self.recur_height(croot.right))
-    
-    def recur_is_balanced(self, croot):
-        b = self.is_balanced(croot)
-        if b is True and croot.parent != None:
-            return self.is_balanced(croot.parent)
-        else:
-            return b
-        # elif isinstance(b, TreeNode)
-        #     print('find lowest unbalanced tree')
-        #     return croot
-    
-    def is_balanced(self, croot):
-        h_left = self.recur_height(croot.left)
-        h_right = self.recur_height(croot.right)
-        if abs(h_left-h_right) <= 1:
-            return True
-        else:
-            return croot   
-    
-    def mirror(self):
-        if self.root is None: return
-        self.recur_mirror(self.root)
-        
-    def recur_mirror(self, croot):
-        if croot is None: return
-        left_child = croot.left
-        croot.left = croot.right
-        croot.right = left_child
-        self.recur_mirror(croot.left)
-        self.recur_mirror(croot.right)
-    
-    def print_paths(self):
-        if self.root is None: return
-        s = ''
-        l = list()
-        self.recur_print_paths(self.root, s, l)
-        return l
-        
-    def recur_print_paths(self, croot, s, l):
-        s = f'{s} {croot.key}'
-        if croot.left is None and croot.right is None:
-            # print(s)
-            l.append(s)
-        if croot.left is not None:
-            self.recur_print_paths(croot.left, s, l)
-        if croot.right is not None:
-            self.recur_print_paths(croot.right, s, l)
 
 # tree = AVL()
 # l = [1,2,4,3,5]
---
+++ dsa/basic_data_structures/test_avl.py
@@ -0,0 +1,100 @@
+import logging
+import os.path
+from avl import AVL
+
+LOGGER = logging.getLogger(__name__)
+
+def test_insert():
+    tree = AVL()
+    s = 10
+    for i in range(0, s):
+        tree.insert(i, i)
+    k = 0
+    curr = tree.root
+    while k < s:
+        assert curr.key == k
+        assert curr.left == None
+        curr = curr.right
+        k += 1
+
+def test_insert2():
+    tree = AVL()
+    s = 100
+    for i in range(0, s):
+        tree.insert(i, i)
+    
+    # try to insert again
+    for i in range(0, 10):
+        tree.insert(i, i+1)
+    k = 0
+    curr = tree.root
+    while k < s:
+        assert curr.key == k
+        assert curr.left == None
+        curr = curr.right
+        k += 1
+
+def test_in_order():
+    tree = AVL()
+    s = 100
+    elem = list()
+    for i in range(0, s):
+        tree.insert(i, i)
+        elem.append(i)
+    res = tree.in_order()
+    assert res == elem
+
+def test_in_order2():
+    tree = AVL()
+    s = 100
+    elem = list()
+    for i in reversed(range(0, s)):
+        tree.insert(i, i)
+        elem.append(i)
+    res = tree.pre_order()
+    assert res == elem
+
+def test_in_order_pre_order_level_order():
+    tree = AVL()
+    l = [38,13,51,10,25,40,84,12,37,66,89,95]
+    length = len(l)
+    while len(l) > 0:
+        key = l.pop(0)
+        value = chr(key)
+        tree.insert(key, value)
+    in_order = tree.in_order()
+    pre_order = tree.pre_order()
+    level_order = tree.level_order()
+    assert in_order ==  [10,12,13,25,37,38,40,51,66,84,89,95]
+    assert pre_order == [38,13,10,12,25,37,51,40,84,66,89,95]
+    assert level_order == [38,13,51,10,25,40,84,12,37,66,89,95]
+
+def test_height():
+    tree = AVL()
+    s = 100
+    assert tree.height() == -1
+    for i in range(0, s):
+        tree.insert(i, i)
+        tree.height() == i
+
+import random
+def test_find():
+    l = list()
+    for i in range(97, 107):
+        l.append(i)
+    
+    tree = AVL()
+    random.shuffle(l)
+    while len(l) > 0:
+        key = l.pop()
+        value = chr(key)
+        tree.insert(key, value)
+    
+    for i in range(97, 107):
+        key = i
+        value = chr(key)
+        croot = tree.find(key)
+        assert croot.value == value
+    
+    croot = tree.find(108)
+    print(croot.key)
