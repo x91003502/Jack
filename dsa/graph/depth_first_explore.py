29e78ddacb1bc69f2514a40b1eebdd6ea246811e
x91003502 <x91003502@gmail.com>
2021-05-23 00:48:27 +0800

    Added depth_first_explore.py.

---
+++ dsa/graph/depth_first_explore.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+from dsa.graph.graph_representation import add_vertex, add_edge, print_graph
+def depth_first_explore(G, visited, v):
+    visited[v] = True
+    print(f'mark vertex : "{v}" visited')
+    if G[v]:
+        for e in G[v]:
+            u, weight = e[0], e[1]
+            if visited[u] == False:
+                print(f'try to explore vertex "{v}" neighbor : vertex "{u}"')
+                depth_first_explore(G, visited, u)
+
+G = defaultdict(str)
+add_vertex(G, 'a')
+add_vertex(G, 'b')
+add_vertex(G, 'c')
+add_vertex(G, 'd')
+add_vertex(G, 'e')
+
+
+add_edge(G, 'a', 'b')
+add_edge(G, 'b', 'a')
+
+add_edge(G, 'b', 'c')
+add_edge(G, 'c', 'b')
+
+add_edge(G, 'c', 'd')
+add_edge(G, 'd', 'c')
+
+
+add_edge(G, 'd', 'e')
+add_edge(G, 'e', 'd')
+
+# print_graph(G)
+
+visited = dict()
+visited['a'] = False
+visited['b'] = False
+visited['c'] = False
+visited['d'] = False
+visited['e'] = False
+
+
+depth_first_explore(G, visited, 'a')
--- dsa/graph/graph_representation.py
+++ dsa/graph/graph_representation.py
@@ -16,20 +16,10 @@ def add_edge(G, v, u, weight=0):
 
 def print_graph(G):
     for v in G:
-        print(f'Vertex : {v}')
+        print(f'vertex : {v}')
         if G[v]:
             for e in G[v]:
                 u, weight = e[0], e[1]
                 print(f'    {v} -> {u}    |    edge weight : {weight}')
         else:
             print(f'    no edge')
-
-G = defaultdict(str)
-add_vertex(G, 'a')
-add_vertex(G, 'b')
-
-add_edge(G, 'a', 'b')
-add_edge(G, 'a', 'c')
-
-print_graph(G)
-
