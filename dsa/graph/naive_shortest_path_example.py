a01ddb677c6d36de67d660d3071220226e93ca17
x91003502 <x91003502@gmail.com>
2021-06-01 01:14:28 +0800

    Added negative edge example.

--- dsa/graph/naive_shortest_path_example.py
+++ dsa/graph/naive_shortest_path_example.py
@@ -4,11 +4,11 @@ from dsa.graph.utils.graph_representation import add_vertex, add_edge
 from dsa.graph.utils.shortest_path import naive_shortest_path
 
 G = defaultdict(str)
+add_vertex(G, 'S')
 add_vertex(G, 'a')
 add_vertex(G, 'b')
 add_vertex(G, 'c')
 add_vertex(G, 'd')
-add_vertex(G, 'S')
 
 
 
@@ -30,3 +30,32 @@ add_edge(G, 'S', 'b', weight=1)
 
 dist = naive_shortest_path(G, 'S')
 print(dist)
+
+G = defaultdict(str)
+add_vertex(G, 's')
+add_vertex(G, 't')
+add_vertex(G, 'x')
+add_vertex(G, 'y')
+add_vertex(G, 'z')
+
+add_edge(G, 's', 't', weight=6)
+add_edge(G, 's', 'y', weight=7)
+
+add_edge(G, 't', 'y', weight=8)
+add_edge(G, 't', 'x', weight=5)
+add_edge(G, 't', 'z', weight=-4)
+
+add_edge(G, 'x', 't', weight=-2)
+
+add_edge(G, 'y', 'z', weight=9)
+add_edge(G, 'y', 'x', weight=-3)
+
+add_edge(G, 'z', 'x', weight=2)
+add_edge(G, 'z', 's', weight=7)
+
+dist = naive_shortest_path(G, 's')
+
+
+
+
+
