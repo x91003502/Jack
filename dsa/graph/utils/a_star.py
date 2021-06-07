9efae1dbf9ee325911e51c87dd188395f5aa880f
x91003502 <x91003502@gmail.com>
2021-06-07 21:51:50 +0800

    Fixed a bug of values stored in distance dictionary.

--- dsa/graph/utils/a_star.py
+++ dsa/graph/utils/a_star.py
@@ -14,38 +14,35 @@ def manhattan_distance(Map, v, t):
     tx, ty = t_pos[0], t_pos[1]
     return abs(vx-tx) + abs(vy-ty)
 
-def potential_edge_weight(edge_wegiht, Map, u, v, t, potential_function='euclidean_distance'):
-    if potential_function == 'euclidean_distance':
-        new_edge_weight = edge_wegiht - euclidean_distance(Map, u, t) + euclidean_distance(Map, v, t)
-    elif potential_function == 'manhattan_distance':
-        new_edge_weight = edge_wegiht - manhattan_distance(Map, u, t) + manhattan_distance(Map, v, t)
-    return new_edge_weight
-
 import sys
 import heapq
-def a_star(G, Map, s, t):
+def a_star(G, Map, s, t, heuristic='euclidean'):
     dist, prev, proc = init_shortest_path(G, s)
     
     pq = init_pq(dist)
     
     while proc.get(t) is False:
-        # If v pop from heap is already processed before, ignore it and pop again.
         distance, v = extract_min(pq, proc)
         print(f'found minimum score vertex {v} with score : {distance}')
         
         for e in G[v]:
             w, weight = e[0], e[1]
-            # new_dist = dist[v] + weight
-            new_dist = dist[v] + weight + euclidean_distance(Map, w, t)
-            # new_dist = dist[v] + weight + manhattan_distance(Map, w, t)
-            
+            new_dist = dist[v] + weight
             if new_dist < dist[w]:
                 old_dist = dist[w]
                 dist[w] = new_dist
+                
+                if heuristic == None:
+                    potential = new_dist
+                elif heuristic == 'euclidean':
+                    potential = new_dist + euclidean_distance(Map, w, t)
+                elif heuristic == 'manhattan':
+                    potential = new_dist + manhattan_distance(Map, w, t)
+                
                 # update the new estimate distance of a vertex by adding a new item in heap
                 # because the new distance must be smaller than previous, it will on the upper of the heap
                 # and will be obtained before the old distance.
-                heapq.heappush(pq, (new_dist, w))
+                heapq.heappush(pq, (potential, w)) # pushing the potential of a vertex in pq instead
                 prev[w] = v
                 print(f'update distance of vertex "{w}" from {old_dist} to {new_dist}')
         proc[v] = True
@@ -69,6 +66,7 @@ def init_pq(dist):
     return pq
 
 def extract_min(pq, proc):
+    # If v pop from heap is already processed before, ignore it and pop again.
     while True:
         tup = heapq.heappop(pq)
         distance, v = tup[0], tup[1]
